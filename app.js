20849fe999d7cd953e09e4c6ae93feba12964061
Felipe Bellas <101675032+felipeBellas@users.noreply.github.com>
2026-09-16 16:44:08 -0300

    Refactor video mirroring logic for clarity

--- app.js
+++ app.js
@@ -7547,17 +7547,6 @@ await new Promise(
 
 
 /*
-  TESTE 4:
-  define o espelhamento ANTES
-  de iniciar a reprodução do novo stream.
-*/
-video.classList.toggle(
-  "mirror",
-  requestedFacingMode ===
-    "user"
-);
-
-/*
   Inicia a reprodução do NOVO stream.
 */
 await video.play();
@@ -7599,7 +7588,14 @@ video.style.objectFit = "cover";
 
 void video.offsetHeight;
 
-
+/*
+  Espelhamento somente na frontal.
+*/
+video.classList.toggle(
+  "mirror",
+  requestedFacingMode ===
+    "user"
+);
 
 /*
   Aguarda um pouco para observar
